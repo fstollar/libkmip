a413d1784f7ed62121ace86886a58685c42a328b
Frank Stollar <frank@stollar.info>
2025-10-13 23:04:28 +0200

    Added missing header definitions

--- include/kmip.h
+++ include/kmip.h
@@ -1574,7 +1574,8 @@ void kmip_set_error_message(KMIP *, const char *);
 int kmip_is_tag_next(const KMIP *, enum tag);
 int kmip_is_tag_type_next(const KMIP *, enum tag, enum type);
 size_t kmip_get_num_items_next(KMIP *, enum tag);
-uint32 kmip_peek_tag(KMIP *);
+uint32 kmip_peek_tag(KMIP *ctx);
+uint32 kmip_skip_tag(KMIP *ctx);
 int kmip_is_attribute_tag(uint32);
 
 /*
